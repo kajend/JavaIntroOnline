2b960b5a076188b47566f1abf7e3f134bc7261fb
kajend <51089958+kajend@users.noreply.github.com>
2019-07-20 01:23:15 +0300

    Update Basics2.java

--- Basics of software code development/Basics/Basics2.java
+++ Basics of software code development/Basics/Basics2.java
@@ -1,15 +1,23 @@
-package by.etc.basics.second_exercise;
+package by.etc.basic.second_exercise;
 
 //Вычислить значение выражения по формуле
 // (все переменные принимают действительные значения): (𝑏+√(𝑏2+4𝑎𝑐))/2𝑎−𝑎3𝑐+𝑏−2
 
 import java.util.Scanner;
 
-public class Basics2 {
+public class Basic2 {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter a");
+
+        while (scan.hasNextDouble() == false) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected DOUBLE type. Try again");
+        }
+
         double a = scan.nextDouble();
 
         while (a == 0) {
@@ -18,6 +26,12 @@ public class Basics2 {
         }
 
         System.out.println("Enter b");
+
+        while (scan.hasNextDouble() == false) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected DOUBLE type. Try again");
+        }
+
         double b = scan.nextDouble();
 
         while (b == 0) {
@@ -26,7 +40,14 @@ public class Basics2 {
         }
 
         System.out.println("Enter c");
+
+        while (scan.hasNextDouble() == false) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected DOUBLE type. Try again");
+        }
+
         double c = scan.nextDouble();
+
         System.out.println("The value is: "
                 + ((b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - a * a * a * c + 1 / (b * b)));
     }
