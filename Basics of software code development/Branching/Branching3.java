7a16cefa5987365c89923b6a434f8d0e922c35f0
kajend <51089958+kajend@users.noreply.github.com>
2019-07-20 23:25:58 +0300

    Update Branching3.java

--- Basics of software code development/Branching/Branching3.java
+++ Basics of software code development/Branching/Branching3.java
@@ -5,10 +5,10 @@ package by.etc.branching.third_exercise;
 
 //Аксиома: через любые две точки проходит прямая, и притом только одна.
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Branching3 {
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -16,54 +16,88 @@ public class Branching3 {
         double k, b; //Уравнение прямой y = kx + b,
         // k - угловой коэффициент, b - свободный член
 
-        try {
-            double x1 = scan.nextDouble();
-            System.out.println("Enter y1");
-            double y1 = scan.nextDouble();
-            System.out.println("Enter x2");
-            double x2 = scan.nextDouble();
-            System.out.println("Enter y2");
-            double y2 = scan.nextDouble();
-            System.out.println("Enter x3");
-            double x3 = scan.nextDouble();
-            System.out.println("Enter y3");
-            double y3 = scan.nextDouble();
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
 
-            if (x1 == x2 && y1 == y2 && x1 == x3 && y1 == y3 && x2 == x3 && y2 == y3) {
-                //Если координаты всех точек совпадают
-                System.out.println("This is one point");
+        double x1 = scan.nextDouble();
 
-            } else {
+        System.out.println("Enter y1");
 
-                if (x1 != x2 && y1 != y2 && x1 != x3 && y1 != y3 && x2 != x3 && y2 != y3) {
-                    //Если координаты всех точек разные
-                    k = (y2 - y1) / (x2 - x1);
-                    b = y1 - x1 * k;
-                    if (y3 == k * x3 + b) {
-                        System.out.println("The points lie on the same line");
-                    } else {
-                        System.out.println("The points don't lie on the same line");
-                    }
-                }
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
 
-                if (x1 == x2 && y1 == y2) { //Если точки А и В совпадают
-                    System.out.println("The points lie on the same line, points A and B coincide");
-                }
+        double y1 = scan.nextDouble();
 
-                if (x1 == x3 && y1 == y3) { //Если точки А и С совпадают
+        System.out.println("Enter x2");
 
-                    System.out.println("The points lie on the same line, points A and C coincide");
-                }
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
 
-                if (x2 == x3 && y2 == y3) { //Если точки В и С сопадают
+        double x2 = scan.nextDouble();
 
-                    System.out.println("The points lie on the same line, points B and C coincide");
+        System.out.println("Enter y2");
+
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
+
+        double y2 = scan.nextDouble();
+
+        System.out.println("Enter x3");
+
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
+
+        double x3 = scan.nextDouble();
+
+        System.out.println("Enter y3");
+
+        while (!scan.hasNextDouble()) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
+
+        double y3 = scan.nextDouble();
+
+        if (x1 == x2 && y1 == y2 && x1 == x3 && y1 == y3 && x2 == x3 && y2 == y3) {
+            //Если координаты всех точек совпадают
+            System.out.println("This is one point");
+
+        } else {
+
+            if (x1 != x2 && y1 != y2 && x1 != x3 && y1 != y3 && x2 != x3 && y2 != y3) {
+                //Если координаты всех точек разные
+                k = (y2 - y1) / (x2 - x1);
+                b = y1 - x1 * k;
+                if (y3 == k * x3 + b) {
+                    System.out.println("The points lie on the same line");
+                } else {
+                    System.out.println("The points don't lie on the same line");
                 }
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Entered number doesn't match the expected type");
+
+            if (x1 == x2 && y1 == y2) { //Если точки А и В совпадают
+                System.out.println("The points lie on the same line, points A and B coincide");
+            }
+
+            if (x1 == x3 && y1 == y3) { //Если точки А и С совпадают
+
+                System.out.println("The points lie on the same line, points A and C coincide");
+            }
+
+            if (x2 == x3 && y2 == y3) { //Если точки В и С сопадают
+
+                System.out.println("The points lie on the same line, points B and C coincide");
+            }
         }
     }
 }
-
-
