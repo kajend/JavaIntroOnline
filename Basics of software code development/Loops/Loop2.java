dca67d007d1a397841e5febd2a00a17ffabe1846
kajend <51089958+kajend@users.noreply.github.com>
2019-07-21 20:33:00 +0300

    Update Loop2.java

--- Basics of software code development/Loops/Loop2.java
+++ Basics of software code development/Loops/Loop2.java
@@ -8,6 +8,7 @@ public class Loop2 {
 
     public static void main(String[] args) {
 
+        @SuppressWarnings("resource")
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a");
