ea149a789d91dfeb0c158e8d3479a19d78a229e1
kajend <51089958+kajend@users.noreply.github.com>
2019-07-21 20:13:35 +0300

    Update Loop3.java

--- Basics of software code development/Loops/Loop3.java
+++ Basics of software code development/Loops/Loop3.java
@@ -3,7 +3,9 @@ package by.etc.loop.third_exercise;
 //Найти сумму квадратов первых ста чисел.
 
 public class Loop3 {
+    
     public static void main(String[] args) {
+        
         int sum = 0;
 
         for (int i = 1; i <= 100; i++) {
