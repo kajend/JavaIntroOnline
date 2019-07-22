741225c53ea7d2668c26ce1099b10bbcd293925e
kajend <51089958+kajend@users.noreply.github.com>
2019-07-22 21:47:51 +0300

    Update and rename One_dimensional_arrays2.java to One_dimensional_array2.java

---
+++ Algorithmization/One dimensional arrays/One_dimensional_array2.java
@@ -0,0 +1,76 @@
+package by.etc.one_dimensional_array.second_exercise;
+
+//Дана последовательность действительных чисел а1 ,а2 ,..., ап.
+// Заменить все ее члены, большие данного Z, этим числом.
+// Подсчитать количество замен.
+
+import java.util.Scanner;
+
+public class One_dimensional_array2 {
+
+    public static void main(String[] args) {
+
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter number");
+
+        while (scan.hasNextDouble() == false) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Try again");
+        }
+
+        double z = scan.nextDouble();
+
+        System.out.println("Enter array size");
+
+        while (scan.hasNextDouble() == false) {
+            scan.next();
+            System.out.println("Entered number doesn't match the expected type. Enter array size");
+        }
+
+        int n = scan.nextInt();
+
+        while (n <= 0) { //Размер массива должен быть положительный
+            System.out.println("Entered number doesn't match the expected type. Enter POSITIVE array size");
+
+            while (scan.hasNextInt() == false) {
+                scan.next();
+                System.out.println("Entered number doesn't match the expected type. Enter array size");
+            }
+
+            n = scan.nextInt();
+        }
+
+        double[] array = new double[n];
+
+        for (int i = 0; i < n; i++) { //Заполнение массива
+            System.out.println("Enter " + i + " element");
+
+            while (scan.hasNextDouble() == false) {
+                scan.next();
+                System.out.println("Entered number doesn't match the expected type. Try again");
+            }
+
+            array[i] = scan.nextDouble();
+        }
+        System.out.print("\nArray: "); //Вывод исходного массива
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        int replacement = 0;
+        System.out.print("\nNew array: ");
+
+        for (int i = 0; i < n; i++) { //Замена(с подсчетом количества) + вывод нового массива
+
+            if (array[i] > z) {
+                array[i] = z;
+                replacement++;
+            }
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nNumber of replacement: " + replacement);
+    }
+}
